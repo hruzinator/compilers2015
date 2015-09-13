0f61686769db97c898c79459beee3b0c41cf8553
Matthew Hruz <matthew.hruz@gmail.com>
2015-09-13 18:32:29 -0500

    Added input files to test code to the standards of the project 1 description

---
+++ src/goodSourceTest.pas
@@ -0,0 +1,11 @@
+Program example(input, output):
+	var x, y: integer;
+	functinon gcd(a, b: integer): integer;
+	begin
+		if b = 0 then gcd := a
+		else gcd := gcd(b, a mod b)
+	end;
+	begin
+		read(x, y);
+		write(gcd(x, y))
+	end.
