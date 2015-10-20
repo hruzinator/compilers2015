e6777c7bd31a1f46af7562b23b0d3e58834fa29c
Matthew Hruz <matthew.hruz@gmail.com>
2015-10-20 17:38:02 -0500

    Made some changes to lexemes and fleshed out the lexeme stats generator

--- docs/lexemeStats.sh
+++ docs/lexemeStats.sh
@@ -1,3 +1,33 @@
-echo "Token types"
+echo "\nToken types"
 echo "-----------"
 grep -E -o '\b[A-Z]+\b' terminals.txt | sort | uniq
+
+echo "\nADDOPS"
+echo "-----------"
+grep -E '\bADDOP\b' terminals.txt | sort | uniq
+
+echo "\nMULTOPS"
+echo "-----------"
+grep -E '\bMULTOP\b' terminals.txt | sort | uniq
+
+echo "\nASSIGNOP"
+echo "-----------"
+grep -E '\bASSIGNOP\b' terminals.txt | sort | uniq
+
+echo "\nIDENTIFIER"
+echo "-----------"
+grep -E '\bIDENTIFIER\b' terminals.txt | sort | uniq
+
+echo "\nSYMBOLS"
+echo "-----------"
+grep -E '\bSYMBOL\b' terminals.txt | sort | uniq
+
+echo "\nNUMBERS"
+echo "-----------"
+grep -E '\bNUMBER\b' terminals.txt | sort | uniq
+
+echo "\nKEYWORDS"
+echo "-----------"
+grep -E '\bKEYWORD\b' terminals.txt | sort | uniq
+
+echo ""
