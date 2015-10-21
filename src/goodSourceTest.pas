40b6dcb7cc9a8398f468429598a1db1b2f5800b7
Matthew Hruz <matthew.hruz@gmail.com>
2015-10-20 21:02:51 -0500

    resolved fpc compile errors in goodSourceTest.pas

--- src/goodSourceTest.pas
+++ src/goodSourceTest.pas
@@ -1,7 +1,7 @@
 {This is a comment. It should be left out of the list of lexemes}
-program example(input, output):
+program example(input, output);
 	var x, y: integer;
-	functinon gcd(a, b: integer): integer;
+	function gcd(a, b: integer): integer;
 	begin
 		if b = 0 then gcd := a
 		else gcd := gcd(b, a mod b)
